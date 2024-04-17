833d3a3f66afbcda6a27c202a2ae875a64d92b8d
amitsingh6742@gmail.com <amitsingh6742@gmail.com>
2024-04-17 10:29:19 +0530

    authantication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,43 @@
-import ErrorHandler from "../middlewares/error.js"
-import {catchAsyncErrors} from "../middlewares/catchAsyncError.js"
-import { User } from "../models/userSchema.js"
-export const  register = catchAsyncErrors(async(req, res, next)=>{
-  const {name, email, phone, role, password} = req.body
-  if(!name || !email || !phone || !role || !password){
-    return next(new ErrorHandler("please fill registration form!"))
+import ErrorHandler from "../middlewares/error.js";
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
+import { User } from "../models/userSchema.js";
+import { sendToken } from "../utils/jwtToken.js";
+export const register = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, phone, role, password } = req.body;
+  if (!name || !email || !phone || !role || !password) {
+    return next(new ErrorHandler("please fill registration form!"));
   }
- const isEmail = await User.findOne({email})
- if(isEmail){
-  return next(new ErrorHandler("email aleady exist"))
- }
- const user = await User.create({
-  name, email, phone, role, password
- })
- res.status(200).json({
-  success: true,
-  message: "user registered successfully",
-  user
- })
+  const isEmail = await User.findOne({ email });
+  if (isEmail) {
+    return next(new ErrorHandler("email aleady exist"));
+  }
+  const user = await User.create({
+    name,
+    email,
+    phone,
+    role,
+    password,
+  });
+  sendToken(user, 200, res, "Registered successfully");
+});
+
+
+export const login = catchAsyncErrors(async(req, res, next)=>{
+   const {email, password, role} = req.body
+   if(!email || !password || !role){
+    return next(new ErrorHandler("please fill login form", 400))
+   }
+   const user = await User.findOne({email}).select("+password")
+   if(!user){
+   return next( new ErrorHandler("invalid email password", 400))
+   }
+   const isPasswordMatch = await user.comparePassword(password) 
+   if(!isPasswordMatch){
+     return next (new ErrorHandler("invalid email password", 400))
+
+   }
+   if(user.role !== role){
+     return next (new ErrorHandler("user with this role not found", 400))
+   }
+   sendToken(user, 200, res, "login successfully")
 })
--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,6 +1,15 @@
 export const sendToken = (user, StatusCode, res, message) => {
   const token =  user.getJWTToken()
   const options = {
-    expires: new Date
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
   }
+  res.status(StatusCode).cookie("token", token, options).json({
+    success: true,
+    message,
+    user,
+    token
+  })
 }
